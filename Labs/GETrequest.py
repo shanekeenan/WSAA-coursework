c7ee749003ca5fd34fac4da202301c5fdf208f77
shanekeenan <10012926@aty.ie>
2023-11-17 21:50:58 +0000

    with lab folder

--- Labs/GETrequest.py
+++ Labs/GETrequest.py
@@ -1,9 +1,13 @@
+
+
 import requests
 import json
 
 url = "http://www.githup.com"
 response = requests.get(url)
 print(response.status_code)
+
+
 
 #if response.status_code == 200: 
 #    page_content = response.text
